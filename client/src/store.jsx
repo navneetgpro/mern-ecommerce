db602d7922e416191277ac596feadf97748df117
Navneet Gupta <navneetgupta004@gmail.com>
2023-04-15 17:35:29 +0530

    Update store.jsx

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,24 +1,37 @@
 import { legacy_createStore as createStore,applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-// const initialState = {};
+const initialState = {};
 
-const initialState = localStorage.getItem('reduxState') 
-                       ? JSON.parse(localStorage.getItem('reduxState'))
-                       : {}
+// const initialState = localStorage.getItem('reduxState') 
+//                        ? JSON.parse(localStorage.getItem('reduxState'))
+//                        : {}
+
+const persistConfig = {
+    key: 'root',
+    whitelist: ['cart'],
+    storage,
+  }
 
 const middleware = [thunk];
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
 
-store.subscribe(()=>{
-    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
-})
+// store.subscribe(()=>{
+//     localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+// })
 
-export default store;
+const persistor = persistStore(store);
+
+export {store, persistor}
+
+// export default store;
